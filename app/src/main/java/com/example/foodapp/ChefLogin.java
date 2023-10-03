41b921e647f782badcaac7ca1380a1c5e5592787
Huy Truong <tnhuy1092003@gmail.com>
2023-10-03 16:49:20 +0700

    add sign up Email and Phone class

---
+++ app/src/main/java/com/example/foodapp/ChefLogin.java
@@ -0,0 +1,14 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChefLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_login);
+    }
+}
