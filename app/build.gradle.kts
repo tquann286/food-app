1aeb1e56f270c249066e417ce9d2d47df7e3ab50
tquann286 <quantrung286@gmail.com>
2023-10-03 12:27:12 +0700

    Setup base file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -12,6 +13,7 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables.useSupportLibrary = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -30,10 +32,32 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.1.2")
+    implementation("com.google.firebase:firebase-database:20.2.2")
+    implementation("com.google.firebase:firebase-storage:20.2.1")
+    implementation("com.google.firebase:firebase-messaging:23.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.6.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.1")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
+    implementation("androidx.navigation:navigation-fragment:2.3.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.cepheuen.elegant-number-button:lib:1.0.2")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("com.firebaseui:firebase-ui-database:6.2.1")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
+    implementation("com.theartofdev.edmodo:android-image-cropper:2.3.+")
+    implementation("androidx.navigation:navigation-ui:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("com.hbb20:ccp:2.3.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 }
