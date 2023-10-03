fde0dfbe6734995c84151786c13b50b1570054f1
tquann286 <quantrung286@gmail.com>
2023-10-03 16:10:05 +0700

    implement layout for choose user type

--- app/src/main/java/com/example/foodapp/MainMenu.java
+++ app/src/main/java/com/example/foodapp/MainMenu.java
@@ -2,13 +2,63 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.os.Bundle;
 
 public class MainMenu extends AppCompatActivity {
+
+    Button signInEmail,signInPhone,signUp;
+    ImageView bgImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        signInEmail=(Button)findViewById(R.id.SignwithEmail);
+        signInPhone=(Button)findViewById(R.id.SignwithPhone);
+        signUp=(Button)findViewById(R.id.Signup);
+
+        signInEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signEmail = new Intent(MainMenu.this,ChooseOne.class);
+                signEmail.putExtra("Home","Email");
+                startActivity(signEmail);
+                finish();
+            }
+        });
+
+        signInPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signPhone = new Intent(MainMenu.this,ChooseOne.class);
+                signPhone.putExtra("Home","Phone");
+                startActivity(signPhone);
+                finish();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(MainMenu.this,ChooseOne.class);
+                signUp.putExtra("Home","SignUp");
+                startActivity(signUp);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
     }
 }
