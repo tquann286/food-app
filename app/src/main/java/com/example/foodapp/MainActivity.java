9a7a8b1eff88a1f022e3fa0d17fd3570d82c09a0
tquann286 <quantrung286@gmail.com>
2023-10-03 15:38:43 +0700

    implement layout for main menu

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,17 +2,44 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void Hello() {
-        System.out.println("Hello");
+        imageView = (ImageView)findViewById(R.id.imageView);
+        imageTextView = (ImageView)findViewById(R.id.imageTextView7);
+
+        imageView.animate().alpha(0f).setDuration(0);
+        imageTextView.animate().alpha(0f).setDuration(0);
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                imageTextView.animate().alpha(1f).setDuration(800);
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
     }
 }
