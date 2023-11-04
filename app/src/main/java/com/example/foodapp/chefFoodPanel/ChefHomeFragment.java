4ac9086d1580b8890c358b07bbaf9c6e443bca57
tquann286 <quantrung286@gmail.com>
2023-11-05 01:16:24 +0700

    Firebase Sign Out

--- app/src/main/java/com/example/foodapp/chefFoodPanel/ChefHomeFragment.java
+++ app/src/main/java/com/example/foodapp/chefFoodPanel/ChefHomeFragment.java
@@ -1,13 +1,21 @@
 package com.example.foodapp.chefFoodPanel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.example.foodapp.MainMenu;
 import com.example.foodapp.R;
 
 public class ChefHomeFragment extends Fragment {
@@ -17,6 +25,32 @@ public class ChefHomeFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_chef_home,null);
         getActivity().setTitle("Trang chủ");
+        setHasOptionsMenu(true);
+        
         return v;
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.logout,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int idd = item.getItemId();
+        if(idd == R.id.LOGOUT){
+            Logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void Logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), MainMenu.class);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/foodapp/chefFoodPanel/chefPostDish.java
@@ -0,0 +1,16 @@
+package com.example.foodapp.chefFoodPanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodapp.R;
+
+public class chefPostDish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_post_dish);
+    }
+}
