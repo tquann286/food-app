4908bdf3f27b83469636c3ba0b8227c9fccdd2d9
tquann286 <quantrung286@gmail.com>
2023-11-04 18:23:53 +0700

    customer registration

--- app/src/main/java/com/example/foodapp/Registration.java
+++ app/src/main/java/com/example/foodapp/Registration.java
@@ -1,14 +1,339 @@
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hbb20.CountryCodePicker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Registration extends AppCompatActivity {
+
+    TextInputLayout Fname,Lname,Email,Pass,cpass,mobileno,localaddress,area,pincode;
+    Spinner Statespin,Cityspin;
+    Button signup, Emaill, Phone;
+    CountryCodePicker Cpp;
+    FirebaseAuth FAuth;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    String fname,lname,emailid,password,confpassword,mobile,Localaddress,Area,Pincode,statee,cityy;
+    String role="Customer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Map<String, List<String>> state = new HashMap<>();
+
+        List<String> haNoiList = new ArrayList<>();
+        haNoiList.add("Quận Hoàn Kiếm");
+        haNoiList.add("Quận Ba Đình");
+        haNoiList.add("Quận Tây Hồ");
+
+        List<String> thuaThienHueList = new ArrayList<>();
+        thuaThienHueList.add("Thành phố Huế");
+        thuaThienHueList.add("Huyện Nam Đông");
+        thuaThienHueList.add("Huyện Phong Điền");
+
+        List<String> saiGonList = new ArrayList<>();
+        saiGonList.add("Quận 1");
+        saiGonList.add("Quận Bình Thạnh");
+        saiGonList.add("Quận Tân Bình");
+
+        state.put("Hà Nội", haNoiList);
+        state.put("Thừa Thiên Huế", thuaThienHueList);
+        state.put("TP. Hồ Chí Minh", saiGonList);
+
+        Fname = (TextInputLayout)findViewById(R.id.Fname);
+        Lname = (TextInputLayout)findViewById(R.id.Lname);
+        Email = (TextInputLayout)findViewById(R.id.Emailid);
+        Pass = (TextInputLayout)findViewById(R.id.Password);
+        cpass = (TextInputLayout)findViewById(R.id.confirmpass);
+        mobileno = (TextInputLayout)findViewById(R.id.Mobilenumber);
+        localaddress = (TextInputLayout)findViewById(R.id.Localaddress);
+        pincode = (TextInputLayout)findViewById(R.id.Pincode);
+        Statespin = (Spinner) findViewById(R.id.Statee);
+        Cityspin = (Spinner) findViewById(R.id.Citys);
+        area = (TextInputLayout)findViewById(R.id.Area);
+
+        signup = (Button)findViewById(R.id.button);
+        Emaill = (Button)findViewById(R.id.email);
+        Phone = (Button)findViewById(R.id.phone);
+
+        Cpp = (CountryCodePicker)findViewById(R.id.CountryCode);
+
+        Statespin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Object value = parent.getItemAtPosition(position);
+                statee = value.toString().trim();
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(Registration.this,android.R.layout.simple_spinner_item,state.get(statee));
+                Cityspin.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Cityspin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object value = parent.getItemAtPosition(position);
+                cityy = value.toString().trim();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        databaseReference = firebaseDatabase.getInstance().getReference("Customer");
+        FAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fname = Fname.getEditText().getText().toString().trim();
+                lname = Lname.getEditText().getText().toString().trim();
+                emailid = Email.getEditText().getText().toString().trim();
+                mobile = mobileno.getEditText().getText().toString().trim();
+                password = Pass.getEditText().getText().toString().trim();
+                confpassword = cpass.getEditText().getText().toString().trim();
+                Area = area.getEditText().getText().toString().trim();
+                Localaddress = localaddress.getEditText().getText().toString().trim();
+                Pincode = pincode.getEditText().getText().toString().trim();
+
+                if (isValid()){
+                    final ProgressDialog mDialog = new ProgressDialog(Registration.this);
+                    mDialog.setCancelable(false);
+                    mDialog.setCanceledOnTouchOutside(false);
+                    mDialog.setMessage("Đang đăng ký, vui lòng đợi......");
+                    mDialog.show();
+
+                    FAuth.createUserWithEmailAndPassword(emailid,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                String useridd = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                databaseReference = FirebaseDatabase.getInstance().getReference("User").child(useridd);
+                                final HashMap<String , String> hashMap = new HashMap<>();
+                                hashMap.put("Role",role);
+                                databaseReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        HashMap<String , String> hashMap1 = new HashMap<>();
+                                        hashMap1.put("Mobile No",mobile);
+                                        hashMap1.put("First Name",fname);
+                                        hashMap1.put("Last Name",lname);
+                                        hashMap1.put("EmailId",emailid);
+                                        hashMap1.put("City",cityy);
+                                        hashMap1.put("Area",Area);
+                                        hashMap1.put("Password",password);
+                                        hashMap1.put("Pincode",Pincode);
+                                        hashMap1.put("State",statee);
+                                        hashMap1.put("Confirm Password",confpassword);
+                                        hashMap1.put("Local Address",Localaddress);
+
+                                        firebaseDatabase.getInstance().getReference("Customer")
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .setValue(hashMap1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        mDialog.dismiss();
+
+                                                        FAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                                                if(task.isSuccessful()){
+                                                                    AlertDialog.Builder builder = new AlertDialog.Builder(Registration.this);
+                                                                    builder.setMessage("Đăng ký thành công! Hãy kiểm tra email của bạn");
+                                                                    builder.setCancelable(false);
+                                                                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                                        @Override
+                                                                        public void onClick(DialogInterface dialog, int which) {
+
+                                                                            dialog.dismiss();
+
+                                                                            String phonenumber = Cpp.getSelectedCountryCodeWithPlus() + mobile;
+                                                                            Intent b = new Intent(Registration.this,VerifyPhone.class);
+                                                                            b.putExtra("phonenumber",phonenumber);
+                                                                            startActivity(b);
+
+                                                                        }
+                                                                    });
+                                                                    AlertDialog Alert = builder.create();
+                                                                    Alert.show();
+                                                                }else{
+                                                                    mDialog.dismiss();
+                                                                    ReusableCodeForAll.ShowAlert(Registration.this,"Có lỗi xảy ra, vui lòng thử lại sau",task.getException().getMessage());
+                                                                }
+                                                            }
+                                                        });
+
+                                                    }
+                                                });
+
+                                    }
+                                });
+                            }else {
+                                mDialog.dismiss();
+                                ReusableCodeForAll.ShowAlert(Registration.this, "Error", task.getException().getMessage());
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        Emaill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registration.this,Login.class));
+                finish();
+            }
+        });
+        Phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registration.this,LoginPhone.class));
+                finish();
+            }
+        });
+    }
+
+    String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    public boolean isValid(){
+        Email.setErrorEnabled(false);
+        Email.setError("");
+        Fname.setErrorEnabled(false);
+        Fname.setError("");
+        Lname.setErrorEnabled(false);
+        Lname.setError("");
+        Pass.setErrorEnabled(false);
+        Pass.setError("");
+        mobileno.setErrorEnabled(false);
+        mobileno.setError("");
+        cpass.setErrorEnabled(false);
+        cpass.setError("");
+        area.setErrorEnabled(false);
+        area.setError("");
+        localaddress.setErrorEnabled(false);
+        localaddress.setError("");
+        pincode.setErrorEnabled(false);
+        pincode.setError("");
+
+        boolean isValid=false,isValidlocaladd=false,isValidlname=false,isValidname=false,isValidemail=false,isValidpassword=false,isValidconfpassword=false,isValidmobilenum=false,isValidarea=false,isValidpincode=false;
+        if(TextUtils.isEmpty(fname)){
+            Fname.setErrorEnabled(true);
+            Fname.setError("Vui lòng nhập tên");
+        }else{
+            isValidname = true;
+        }
+        if(TextUtils.isEmpty(lname)){
+            Lname.setErrorEnabled(true);
+            Lname.setError("Vui lòng nhập họ");
+        }else{
+            isValidlname = true;
+        }
+        if(TextUtils.isEmpty(emailid)){
+            Email.setErrorEnabled(true);
+            Email.setError("Vui lòng nhập email");
+        }else{
+            if(emailid.matches(emailpattern)){
+                isValidemail = true;
+            }else{
+                Email.setErrorEnabled(true);
+                Email.setError("Email không hợp lệ");
+            }
+        }
+        if(TextUtils.isEmpty(password)){
+            Pass.setErrorEnabled(true);
+            Pass.setError("Vui lòng nhập mật khẩu");
+        }else{
+            if(password.length()<8){
+                Pass.setErrorEnabled(true);
+                Pass.setError("Mật khẩu cần ít nhất 8 ký tự");
+            }else{
+                isValidpassword = true;
+            }
+        }
+        if(TextUtils.isEmpty(confpassword)){
+            cpass.setErrorEnabled(true);
+            cpass.setError("Vui lòng nhập.");
+        }else{
+            if(!password.equals(confpassword)){
+                cpass.setErrorEnabled(true);
+                cpass.setError("Mật khẩu không khớp");
+            }else{
+                isValidconfpassword = true;
+            }
+        }
+        if(TextUtils.isEmpty(mobile)){
+            mobileno.setErrorEnabled(true);
+            mobileno.setError("Vui lòng nhập");
+        }else{
+            if(mobile.length()<9){
+                mobileno.setErrorEnabled(true);
+                mobileno.setError("Số điện thoại ít nhất 9 ký tự");
+            }else{
+                isValidmobilenum = true;
+            }
+        }
+        if(TextUtils.isEmpty(Area)){
+            area.setErrorEnabled(true);
+            area.setError("Vui lòng nhập khu vực");
+        }else{
+            isValidarea = true;
+        }
+        if(TextUtils.isEmpty(Pincode)){
+            pincode.setErrorEnabled(true);
+            pincode.setError("Vui lòng nhập mã Pin");
+        }else{
+            isValidpincode = true;
+        }
+        if(TextUtils.isEmpty(Localaddress)){
+            localaddress.setErrorEnabled(true);
+            localaddress.setError("Vui lòng nhập địa chỉ");
+        }else{
+            isValidlocaladd = true;
+        }
+
+        isValid = (isValidarea && isValidconfpassword && isValidpassword && isValidpincode && isValidemail && isValidmobilenum && isValidname && isValidlocaladd && isValidlname) ? true : false;
+        return isValid;
     }
 }
---
+++ app/src/main/java/com/example/foodapp/SendOtp.java
@@ -0,0 +1,182 @@
+package com.example.foodapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class SendOtp extends AppCompatActivity {
+
+    String verificationId;
+    FirebaseAuth FAuth;
+    Button verify , Resend ;
+    TextView txt;
+    EditText entercode;
+    String phoneno;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_otp);
+
+        phoneno = getIntent().getStringExtra("Phonenumber").trim();
+
+        entercode = (EditText) findViewById(R.id.code);
+        txt = (TextView) findViewById(R.id.text);
+        Resend = (Button)findViewById(R.id.Resendotp);
+        verify = (Button) findViewById(R.id.Verify);
+        FAuth = FirebaseAuth.getInstance();
+
+        Resend.setVisibility(View.INVISIBLE);
+        txt.setVisibility(View.INVISIBLE);
+        sendVerificationCode(phoneno);
+
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String code = entercode.getText().toString().trim();
+                Resend.setVisibility(View.INVISIBLE);
+
+                if (code.isEmpty() && code.length()<6){
+                    entercode.setError("Enter code");
+                    entercode.requestFocus();
+                    return;
+                }
+                verifyCode(code);
+            }
+        });
+
+        new CountDownTimer(60000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                txt.setVisibility(View.VISIBLE);
+                txt.setText("Gửi lại mã trong "+millisUntilFinished/1000+" giây");
+
+            }
+
+            /**
+             * Callback fired when the time is up.
+             */
+            @Override
+            public void onFinish() {
+                Resend.setVisibility(View.VISIBLE);
+                txt.setVisibility(View.INVISIBLE);
+
+            }
+        }.start();
+
+        Resend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Resend.setVisibility(View.INVISIBLE);
+                ResendOtp(phoneno);
+
+                new CountDownTimer(60000,1000){
+
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+
+                        txt.setVisibility(View.VISIBLE);
+                        txt.setText("Gửi lại mã trong "+millisUntilFinished/1000+" giây");
+
+                    }
+
+                    /**
+                     * Callback fired when the time is up.
+                     */
+                    @Override
+                    public void onFinish() {
+                        Resend.setVisibility(View.VISIBLE);
+                        txt.setVisibility(View.INVISIBLE);
+
+                    }
+                }.start();
+            }
+        });
+    }
+
+    private void sendVerificationCode(String number) {
+
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder()
+                .setPhoneNumber(number)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(callBack)
+                .build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callBack=new PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null){
+                entercode.setText(code);  // Auto Verification
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(SendOtp.this , e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(String s , PhoneAuthProvider.ForceResendingToken forceResendingToken){
+            super.onCodeSent(s,forceResendingToken);
+            verificationId = s;
+        }
+    };
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId , code);
+        signInWithPhone(credential);
+    }
+
+    private void signInWithPhone(PhoneAuthCredential credential) {
+
+        FAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SendOtp.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SendOtp.this,ChefFoodPanel_BottomNavigation.class));
+                            finish();
+                        }else{
+                            ReusableCodeForAll.ShowAlert(SendOtp.this,"Lỗi",task.getException().getMessage());
+                        }
+                    }
+                });
+
+    }
+
+    private void ResendOtp(String phonenum) {
+        sendVerificationCode(phonenum);
+    }
+}
