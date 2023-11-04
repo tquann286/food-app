bcc07bee3849c13f3dd11287fe0651f202fb1a7a
tquann286 <quantrung286@gmail.com>
2023-11-04 15:48:48 +0700

    check logged in user

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     ImageView imageTextView;
+    FirebaseAuth Fauth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +49,49 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(MainActivity.this,MainMenu.class);
-                startActivity(intent);
-                finish();
+                Fauth = FirebaseAuth.getInstance();
 
+                if(Fauth.getCurrentUser()!=null) {
+                    if(Fauth.getCurrentUser().isEmailVerified()){
+                        Fauth=FirebaseAuth.getInstance();
+                        databaseReference = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getUid()+"/Role");
+                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                String role = snapshot.getValue(String.class);
+                                if(role.equals("Chef")){
+                                    startActivity(new Intent(MainActivity.this,ChefFoodPanel_BottomNavigation.class));
+                                    finish();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Vui lòng xác minh email của bạn");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                        Fauth.signOut();
+                    }
+                } else {
+                    Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         },3000);
     }
