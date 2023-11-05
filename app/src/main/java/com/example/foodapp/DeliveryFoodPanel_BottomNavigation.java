99ca75c83c8ea47f30323b8a1f9d053091a2e30a
tquann286 <quantrung286@gmail.com>
2023-11-05 23:06:05 +0700

    Bottom Navigation Bar Delivery Person

--- app/src/main/java/com/example/foodapp/DeliveryFoodPanel_BottomNavigation.java
+++ app/src/main/java/com/example/foodapp/DeliveryFoodPanel_BottomNavigation.java
@@ -1,14 +1,48 @@
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.example.foodapp.deliveryFoodPanel.DeliveryPendingOrderFragment;
+import com.example.foodapp.deliveryFoodPanel.DeliveryShipOrderFragment;
+
+public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_food_panel_bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.delivery_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.shiporders) {
+            fragment=new DeliveryShipOrderFragment();
+        } else if (itemId == R.id.pendingorders) {
+            fragment=new DeliveryPendingOrderFragment();
+        }
+
+        return loadDeliveryFragment(fragment);
+    }
+
+    private boolean loadDeliveryFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
