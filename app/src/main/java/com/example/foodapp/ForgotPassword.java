c77d195bbfa5d82e31e7752e0b7a640995889d4d
tquann286 <quantrung286@gmail.com>
2023-11-08 13:08:33 +0700

    food detail customer

--- app/src/main/java/com/example/foodapp/ForgotPassword.java
+++ app/src/main/java/com/example/foodapp/ForgotPassword.java
@@ -1,14 +1,55 @@
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    TextInputLayout forgetpassword;
+    Button Reset;
+    FirebaseAuth FAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        forgetpassword = (TextInputLayout) findViewById(R.id.Emailid);
+        Reset = (Button) findViewById(R.id.button2);
+
+        FAuth = FirebaseAuth.getInstance();
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(ForgotPassword.this);
+                mDialog.setCancelable(false);
+                mDialog.setCanceledOnTouchOutside(false);
+                mDialog.setMessage("Đang xử lý...");
+                mDialog.show();
+
+                FAuth.sendPasswordResetEmail(forgetpassword.getEditText().getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mDialog.dismiss();
+                            ReusableCodeForAll.ShowAlert(ForgotPassword.this, "Thành công", "Hãy kiểm tra email của bạn.");
+                        } else {
+                            mDialog.dismiss();
+                            ReusableCodeForAll.ShowAlert(ForgotPassword.this, "Lỗi", task.getException().getMessage());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
