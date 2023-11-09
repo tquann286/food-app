f463ea8a232f7c56d30ead3b201cb0cfa5abf775
huynhthanhphat <thanhphat02122003@gmail.com>
2023-11-09 16:33:25 +0700

    chef acept order

---
+++ app/src/main/java/com/example/foodapp/chefFoodPanel/Chef_order_dishes.java
@@ -0,0 +1,16 @@
+package com.example.foodapp.chefFoodPanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodapp.R;
+
+public class Chef_order_dishes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_order_dishes);
+    }
+}
