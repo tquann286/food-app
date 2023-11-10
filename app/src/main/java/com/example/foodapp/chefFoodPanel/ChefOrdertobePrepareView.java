362f617c6f62f629b9909cab276ce9da830250b6
tquann286 <quantrung286@gmail.com>
2023-11-10 17:20:08 +0700

    chef pending order detail

--- app/src/main/java/com/example/foodapp/chefFoodPanel/ChefOrdertobePrepareView.java
+++ app/src/main/java/com/example/foodapp/chefFoodPanel/ChefOrdertobePrepareView.java
@@ -10,6 +10,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
